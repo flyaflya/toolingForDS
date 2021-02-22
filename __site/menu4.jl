de70624663cfd8a3b318c59704742b1a57397744
flyaflya <ajf@udel.edu>
2021-02-22 15:22:30 -0500

    larger font

---
+++ __site/menu4.jl
@@ -0,0 +1,14 @@
+### A Pluto.jl notebook ###
+# v0.12.20
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ b673ef20-7540-11eb-20c8-2bca83428010
+md"""
+# This is a heading for testing deploying Pluto
+Can I then write text and continue.  I wodner
+"""
+
+# ╔═╡ Cell order:
+# ╟─b673ef20-7540-11eb-20c8-2bca83428010
