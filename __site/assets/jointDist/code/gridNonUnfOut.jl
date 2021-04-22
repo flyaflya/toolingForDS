12f8cb44fb778d3f02a4c84737579132b6b5b448
flyaflya <ajf@udel.edu>
2021-04-22 17:57:05 -0400

    mcmc

--- __site/assets/jointDist/code/gridNonUnfOut.jl
+++
@@ -1,9 +0,0 @@
-# This file was generated, do not modify it. # hide
-#hideall
-plot(gridDF,
-        x = :winningsMultiplier, 
-        y = :maxWinnings,
-        color = :integrand,
-        Scale.y_continuous(labels = x -> format(x, commas = true)))
-img = SVG(joinpath(@OUTPUT, "gridNonUnif.svg"), 4inch, 3inch)
-draw(img, p)
